6144a5a484e655e5d0e6e09d46d3084855717b7f
SMC242 <56912943+SMC242@users.noreply.github.com>
2026-07-16 23:30:04 +0100

    feat: parse opus header packet
    - Parses the relevant data from the Opus header packet (which is the
    first packet in the ogg container)
      - The channels are not parsed as phone microphones won't be stereo
    anyway => I will demux the stream
    - Skips the tags packet because it's not relevant to this application as
    this is phone audio, not music

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ogg::reading as ogg;
 use std::fs;
+use std::num::NonZeroU8;
 use std::path::Path;
 
 // From https://opus-codec.org/examples/
@@ -8,7 +9,7 @@ const FILE_PATH: &str = "/home/eilidhm/Downloads/ehren-paper_lights-96.opus";
 /// Sampling rate in Hertz
 /// NOTE: the Opus codec only supports these sampling rates
 #[derive(Debug, Clone, Copy)]
-#[repr(u16)]
+#[repr(u32)]
 pub enum SamplingRate {
     Narrowband = 8_000,
     Mediumband = 12_000,
@@ -17,11 +18,36 @@ pub enum SamplingRate {
     Fullband = 48_000,
 }
 
+impl SamplingRate {
+    pub fn to_hertz(&self) -> u32 {
+        *self as u32
+    }
+}
+
+impl TryFrom<u32> for SamplingRate {
+    type Error = &'static str;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        const ALL: &[SamplingRate] = &[
+            SamplingRate::Narrowband,
+            SamplingRate::Mediumband,
+            SamplingRate::Wideband,
+            SamplingRate::SuperWideband,
+            SamplingRate::Fullband,
+        ];
+        ALL.iter().find(|&variant| variant.to_hertz() == value).copied()
+            .ok_or("Invalid sampling rate for the Opus codec. See https://www.rfc-editor.org/info/rfc7845/#section-5.1")
+    }
+}
+
 #[derive(Debug)]
 enum DecodeDtmfError {
     IoError(std::io::Error),
     SignalReadError(DecodeTimesliceError),
+    OpusParseError(OpusHeaderParseError),
 }
+
+type SignalSlice = Vec<i16>;
 
 #[derive(Debug)]
 pub enum DecodeTimesliceError {
@@ -42,7 +68,9 @@ pub fn decode_timeslice(
     timeslice: std::time::Duration,
     decoder: &mut opus::Decoder,
     stream: &mut ogg::PacketReader<fs::File>,
-) -> Result<Vec<i16>, DecodeTimesliceError> {
+) -> Result<SignalSlice, DecodeTimesliceError> {
+    const DECODE_FEC: bool = false;
+
     let mut signal = Vec::new();
     // Assumes no packet loss
     let expected_samples = u64::from(sampling_rate as u16) * timeslice.as_secs();
@@ -53,12 +81,13 @@ pub fn decode_timeslice(
                 dbg!(&packet.data);
                 let mut packet_data: Vec<i16> = vec![0; packet.data.len()];
                 dbg!(&packet_data);
+                println!("{0:x?}", packet.data);
                 dbg!(packet.data.len(), packet_data.len());
 
-                let samples = decoder
-                    .decode(&packet.data, &mut packet_data, true)
+                let frame_size = decoder
+                    .decode(&packet.data, &mut packet_data, DECODE_FEC)
                     .map_err(DecodeTimesliceError::DecodeError)?;
-                total_samples += u64::try_from(samples)
+                total_samples += u64::try_from(frame_size)
                     .expect("Converting sample count to u64 should be ok on 64-bit systems");
                 signal.extend(packet_data);
             }
@@ -67,6 +96,109 @@ pub fn decode_timeslice(
         };
     }
     Ok(signal)
+}
+
+// See https://www.rfc-editor.org/info/rfc7845/#section-5
+
+#[derive(Debug)]
+pub struct OpusStreamMetadata {
+    pub version: u8,
+    pub channel_count: NonZeroU8,
+    pub preskip: u16,
+    pub input_sample_rate_hz: SamplingRate,
+    pub output_gain_db: i16,
+}
+
+#[derive(Debug)]
+pub enum OpusHeaderParseError {
+    MissingOpusHead,
+    MalformedOpusHead(String),
+    UnsupportedVersion(u8),
+    MissingOpusTags,
+    ReadFailed(ogg::OggReadError),
+    /// Only mono and stereo audio is supported as this application focuses on phone calls.
+    /// 5.1 surround sound is unlikely in this case and channel family 255 shouldn't be used by
+    /// "general-purpose players". See https://www.rfc-editor.org/info/rfc7845/#section-5.1.1.1
+    UnsupportedChannelFamily,
+}
+
+fn read_multi_byte<const N: usize, T, E, F: FnOnce([u8; N]) -> T>(
+    converter: F,
+    err: E,
+    bytes: &[u8],
+) -> Result<T, E> {
+    <[u8; N]>::try_from(bytes).map_err(|_| err).map(converter)
+}
+
+fn parse_opus_headers<T: std::io::Read + std::io::Seek>(
+    reader: &mut ogg::PacketReader<T>,
+) -> Result<OpusStreamMetadata, OpusHeaderParseError> {
+    let opus_head_packet = reader
+        .read_packet()
+        .map_err(OpusHeaderParseError::ReadFailed)
+        .map(|p| p.ok_or(OpusHeaderParseError::MissingOpusHead))??;
+
+    // Ignore the minor version as required by the RFC
+    let version = opus_head_packet.data[1] & 0b11110000;
+    if version != 1 {
+        return Err(OpusHeaderParseError::UnsupportedVersion(version));
+    }
+
+    let channel_count = NonZeroU8::try_from(opus_head_packet.data[2]).map_err(|_| {
+        OpusHeaderParseError::MalformedOpusHead("Invalid channel count: 0".to_string())
+    })?;
+
+    // All mutli-byte values will be little endian due to RFC
+    let preskip = read_multi_byte(
+        u16::from_le_bytes,
+        OpusHeaderParseError::MalformedOpusHead(
+            "Unexpected end of stream in preskip header".to_string(),
+        ),
+        &opus_head_packet.data[3..4],
+    )?;
+
+    let input_sample_rate_hz = read_multi_byte(
+        u32::from_le_bytes,
+        OpusHeaderParseError::MalformedOpusHead(
+            "Unexpected end of stream in input sample rate header".to_string(),
+        ),
+        &opus_head_packet.data[5..9],
+    )?;
+    let sampling_rate = SamplingRate::try_from(input_sample_rate_hz).or(Err(
+        OpusHeaderParseError::MalformedOpusHead(format!(
+            "Invalid sampling rate '{input_sample_rate_hz}' for the Opus codec"
+        )),
+    ))?;
+
+    let output_gain_db = read_multi_byte(
+        i16::from_le_bytes,
+        OpusHeaderParseError::MalformedOpusHead(
+            "Unexpected end of stream in output gain header".to_string(),
+        ),
+        &opus_head_packet.data[9..13],
+    )?;
+
+    let _channel_mapping_family = match opus_head_packet.data[14] {
+        0 => 0,
+        1.. => return Err(OpusHeaderParseError::UnsupportedChannelFamily),
+    };
+
+    // I can't be bothered parsing the channel mapping table so I will demux it
+    // which should be okay according to https://www.rfc-editor.org/info/rfc7845/#section-5.1.1
+    // Phone audio is unlikely to be stereo anyway
+
+    // TODO: skip the OpusTags. Not important for this application
+    let _tags_packet = reader
+        .read_packet()
+        .map_err(OpusHeaderParseError::ReadFailed)
+        .map(|p| p.ok_or(OpusHeaderParseError::MissingOpusTags))??;
+    Ok(OpusStreamMetadata {
+        version,
+        channel_count,
+        preskip,
+        input_sample_rate_hz: sampling_rate,
+        output_gain_db,
+    })
 }
 
 fn main() -> Result<(), DecodeDtmfError> {
@@ -78,6 +210,10 @@ fn main() -> Result<(), DecodeDtmfError> {
     let mut decoder = opus::Decoder::new(u32_sampling_rate, opus::Channels::Stereo)
         .expect("Initialising the decoder should succeed");
     let mut stream = read_ogg(Path::new(FILE_PATH)).map_err(DecodeDtmfError::IoError)?;
+
+    let stream_meta = parse_opus_headers(&mut stream).map_err(DecodeDtmfError::OpusParseError)?;
+    dbg!(stream_meta);
+
     let first_10s = decode_timeslice(
         sampling_rate,
         std::time::Duration::from_secs(10),
